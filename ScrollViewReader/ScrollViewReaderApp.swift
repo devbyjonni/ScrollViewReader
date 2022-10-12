3d145e5b96a1a3bfa19c72c857dfc8e7e9068826
Jonni Åkesson <enterthesource@me.com>
2022-10-12 17:00:04 +0200

    Initial Commit

---
+++ ScrollViewReader/ScrollViewReaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrollViewReaderApp.swift
+//  ScrollViewReader
+//
+//  Created by Jonni Akesson on 2022-10-12.
+//
+
+import SwiftUI
+
+@main
+struct ScrollViewReaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
