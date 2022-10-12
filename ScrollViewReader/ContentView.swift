c77ac91f66ab84c665a1159143e2f994369a18c4
Jonni Åkesson <enterthesource@me.com>
2022-10-12 17:22:36 +0200

    Done

--- ScrollViewReader/ContentView.swift
+++ ScrollViewReader/ContentView.swift
@@ -8,14 +8,50 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var scrollToIndex = 0
+    @State var textFieldText = ""
+    
     var body: some View {
+        
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            TextField("Enter a #", text: $textFieldText)
+                .padding()
+                .background(
+                    RoundedRectangle(cornerRadius: 20)
+                        .fill(Color.gray.opacity(0.3))
+                    .frame(height: 50)
+                )
+                .padding()
+                .keyboardType(.numberPad)
+            
+            Button("Show") {
+                if let index = Int(textFieldText) {
+                    scrollToIndex = index
+                }
+            }
+            
+            ScrollView {
+                ScrollViewReader { proxy in
+                    ForEach(0..<50) { index in
+                        Text("Index: \(index)")
+                            .font(.headline)
+                            .frame(height: 200)
+                            .frame(maxWidth: .infinity)
+                            .background(.white)
+                            .cornerRadius(10)
+                            .padding()
+                            .shadow(radius: 10)
+                            .id(index)
+                    }
+                    .onChange(of: scrollToIndex) { newValue in
+                        withAnimation(.spring()) {
+                            proxy.scrollTo(newValue, anchor: .top)
+                        }
+                    }
+                }
+            }
         }
-        .padding()
     }
 }
 
